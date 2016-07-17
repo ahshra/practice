2c3b4f31d087d5567afc3f392bd6e5c995e2af2c
Aaron <ahshraberg@gmail.com>
2016-07-17 14:52:27 -0700

    Add file rappender.py

---
+++ rappender.py
@@ -0,0 +1,16 @@
+# below is a game where er is added to the end of everything.
+
+z= u'\u513f'
+import sys
+sys.stdout.encoding
+'US-ASCII'
+z.encode('utf-8')
+'\xe7\x8c\xab' #some necessary coding to account for variations in terminal environment 
+#that will influence utf-8 and Chinese character output.
+
+original = raw_input('Enter a word and I\'ll put an "er" on the end of it : ')
+if len(original) > 0:
+    new_word = original + z.encode('utf-8')
+    print new_word
+else:
+    print 'empty'
