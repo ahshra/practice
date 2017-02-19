68c7f9ece23106d3c9a66533078ce119dc54fa92
ahshra <ahshraberg@gmail.com>
2017-02-18 19:21:15 -0800

    Update ageteller.py

--- ageteller.py
+++ ageteller.py
@@ -13,24 +13,3 @@ else:
 		what_year = raw_input('What year were you born? ')
 		current_age = int(now.year) - int(what_year)
 		print ('So, your current age is about %s.') % (current_age)
-	
-	#print now
-	#print now.year
-	#print now.month
-	#print now.day
-
-from datetime import datetime
-now = datetime.now()
-
-print 'Today\'s date is %s-%s-%s' % (now.year, now.month, now.day)
-
-from datetime import datetime,time
-now = datetime.now()
-time = datetime.now()
-
-print 'The time is now %s:%s:%s' % (now.hour, now.minute, now.second)
-to_go = raw_input('What time would you like to go? ')
-time_until = float(to_go) - float(time.hour)
-print 'That\'s in %s hours!' % (time_until)
-print '%s/%s/%s %s:%s:%s' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
-print "%02d:%02d:%02d" % (time.hour, time.minute, time.second)
